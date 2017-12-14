5ddd3778451893ad4f6ab5be772b4fa91e55c5bc
hideInbush <caavril@qq.com>
2017-12-14 20:39:10 +0800

    update

--- build/index.js
+++ build/index.js
@@ -18954,29 +18954,6 @@ var Posts = function (_React$Component2) {
             xmlhttp.open("POST", url, true);
             xmlhttp.send(requestParams);
         }
-
-        /*json语法高亮*/
-
-    }, {
-        key: 'syntaxHighlight',
-        value: function syntaxHighlight(json) {
-            json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
-            return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function (match) {
-                var cls = 'number';
-                if (/^"/.test(match)) {
-                    if (/:$/.test(match)) {
-                        cls = 'key';
-                    } else {
-                        cls = 'string';
-                    }
-                } else if (/true|false/.test(match)) {
-                    cls = 'boolean';
-                } else if (/null/.test(match)) {
-                    cls = 'null';
-                }
-                return '<span class="' + cls + '">' + match + '</span>';
-            });
-        }
     }]);
 
     return Posts;
